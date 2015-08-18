3fed8fa38855f928b1c829121f83c9be4360e256
Gaye Bulut <gayegul@gmail.com>
2015-08-18 05:23:30 -0700

    introduce React

--- labelr/src/app.js
+++ labelr/src/app.js
@@ -1,1 +1,10 @@
-console.log('hello world');
+var React = require('react');
+
+var Hello = React.createClass({
+  displayName: 'Hello',
+  render: function() {
+    return <div>Hello, {this.props.name}</div>
+  }
+})
+
+React.render(<Hello name='World!'/>, document.body);
